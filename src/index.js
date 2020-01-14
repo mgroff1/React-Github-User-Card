6974b0dbb0270fd84000ed6fd952cc9c36b1c12d
mgroff1 <mick.groff@gmail.com>
2020-01-14 14:22:56 -0700

    --first-commit--

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React, {Component} from "react";
+import ReactDOM from "react-dom";
+import axios from "axios";
+
+import "./styles.css";
+
+const App = class  extends Component {
+
+  render() {
+    return (
+      <div> Hi</div>
+    );
+  }
+
+}
+
+export default App;
+
+
+ReactDOM.render(<App />, document.getElementById("root"));
